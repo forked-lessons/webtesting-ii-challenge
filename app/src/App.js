d675b594edf0a176f1294f3f3430efde5baf72e6
Daniel <danielstarling4@gmail.com>
2019-04-09 15:22:55 -0700

    MVP complete

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Dashboard from './components/Dashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Lambda At Bat</h1>
+        <Dashboard />
       </div>
     );
   }
--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,9 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'jest-dom/extend-expect';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+afterEach(cleanup);
+
+describe('The Count', () => {
+  describe('balls', () => {
+    it('increments balls', () => {
+      const { getByText, getByTestId } = render(<App />);
+      const button = getByText(/add ball/i);
+      fireEvent.click(button);
+      const selected = getByTestId('balls');
+      expect(selected).toHaveTextContent(/Balls:1/i);
+    });
+    it('increments balls and resets', () => {
+      const { getByText, getByTestId } = render(<App />);
+      const button = getByText(/add ball/i);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      const selected = getByTestId('balls');
+      expect(selected).toHaveTextContent(/Balls:0/i);
+    });
+  });
+  describe('strikes', () => {
+    it('increments strikes', () => {
+      const { getByText, getByTestId } = render(<App />);
+      const button = getByText(/add strike/i);
+      fireEvent.click(button);
+      const selected = getByTestId('strikes');
+      expect(selected).toHaveTextContent(/strikes:1/i);
+    });
+    it('increments strikes and resets', () => {
+      const { getByText, getByTestId } = render(<App />);
+      const button = getByText(/add strike/i);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      const selected = getByTestId('strikes');
+      expect(selected).toHaveTextContent(/strikes:0/i);
+    });
+  });
+  describe('Fouls', () => {
+    it('increments fouls', () => {
+      const { getByText, getByTestId } = render(<App />);
+      const button = getByText(/add foul/i);
+      fireEvent.click(button);
+      const selected = getByTestId('strikes');
+      expect(selected).toHaveTextContent(/strikes:1/i);
+    });
+    it('increments fouls and stops at 2', () => {
+      const { getByText, getByTestId } = render(<App />);
+      const button = getByText(/add foul/i);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      fireEvent.click(button);
+      const selected = getByTestId('strikes');
+      expect(selected).toHaveTextContent(/strikes:2/i);
+    });
+  });
 });
